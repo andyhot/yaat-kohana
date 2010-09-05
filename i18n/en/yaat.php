5fe302ece54a6c771e58bf715b8607c3b2973ef1
Andreas Andreou <andreoua@gmail.com>
2010-09-05 17:38:46 +0300

    standard en l10n

---
+++ i18n/en/yaat.php
@@ -0,0 +1,10 @@
+  <?php
+  $lang = array
+  (
+	'model-username'=>'Username',
+	'model-password'=>'Password',
+	
+	'action-login'=>'Login',
+	
+	'error-incorrect-credentials'=>'Wrong username and/or password',
+  );
